6c44450311ca6bf0a3370ffe70a3aab7b306e143
Thanh_An <vuthanhan3547@gmail.com>
2025-05-31 15:18:05 +0700

    [Add] Test successfull Create Product using Mapster
    Done Section 5

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -15,7 +15,22 @@
 		decimal Price,
 		string ImageFile);
 
-	public class CreateProductEndpoint
+	public class CreateProductEndpoint : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+			{
+				var command = request.Adapt<CreateProductCommand>();
+				var result = await sender.Send(command);
+				var response = result.Adapt<CreateProductResponse>();
+				return Results.Created($"/products/{response.Id}", response);
+			})
+			.WithName("CreateProduct")
+			.Produces<CreateProductResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Create Product")
+			.WithDescription("Create Product");
+		}
 	}
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
